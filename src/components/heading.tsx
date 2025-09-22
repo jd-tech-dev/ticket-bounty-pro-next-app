97e7e8ae30a39a2c5bb5262c15d7d8b22f667503
JD <65872808+jd-tech-dev@users.noreply.github.com>
2025-09-22 17:40:25 +0200

    chore: update horizontal padding for consistency

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -12,7 +12,7 @@ const Heading = ({ title, description, tabs, actions }: HeadingProps) => {
   return (
     <>
       {tabs}
-      <div className="flex items-center justify-between px-8">
+      <div className="flex items-center justify-between px-2">
         <div>
           <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
           {description && (
